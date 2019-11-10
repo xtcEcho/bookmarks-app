6356b0077673e7e8ad995a2125feb1264e85ce47
Wenye Lu <luwenye2010@gmail.com>
2019-11-10 17:23:15 -0500

    updated

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export default {
   API_ENDPOINT: `https://tf-ed-bookmarks-api.herokuapp.com/v3/bookmarks`,
-  API_KEY: '$2a$10$ra1z0n2XnSnbMP/ipTMHeOqqrI7i8Rssm/z8MHTxgb7LamV7LpfXu',
+  API_KEY: '$2a$10$FXjcZO/ypE85MKDDP5AzpuYyp4feh5funz./FQrGJ3o9QQkyHIZmS',
 }
